3ae5422b10059bf5b2bdf2b84a2dc4fc3a1e4878
Kyotho <jakub.jajkowicz@gmail.com>
2019-04-16 23:29:13 +0200

    hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+$(document).ready(function() {
+  navbarCollapse();
+  menuSlidedown();
+});
+
+$(window).resize(function() {
+  navbarCollapse();
+});
+
+function navbarCollapse() {
+  var windowWidth = parseInt($(window).width(), 10);
+  $('.hamburger').hide();
+  if (windowWidth <= 960) {
+    $('.navbar__nav').css('display', 'none');
+    $('.navbar__nav').addClass('show');
+    $('.hamburger').show();
+    $('.nav__item  ').css('display', 'block');
+  } else {
+    $('.navbar__nav').css('display', 'block');
+    $('.navbar__nav').removeClass('show');
+    $('.hamburger').hide();
+    $('.nav__item  ').css('display', 'inline-block');
+  }
+}
+
 // smooth scrolling
 $('a').click(function() {
   var sectionTo = $(this).attr('href');
@@ -9,9 +34,16 @@ $('a').click(function() {
   );
 });
 
+// smooth to top
 function smoothTopButton() {
   $('.arrow__box--up').click(function() {
     $('html, body').animate({ scrollTop: 0 }, 600);
     return false;
+  });
+}
+
+function menuSlidedown() {
+  $('.hamburger').click(function() {
+    $('.navbar__nav').slideToggle('slow');
   });
 }
